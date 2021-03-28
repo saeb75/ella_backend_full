515e1c8ace68ae6675a73af36e2ff9ae3ed2d332
saeb <saebfat@gmail.com>
2021-03-29 00:44:02 +0430

    make it better

--- src/Routes/color.js
+++ src/Routes/color.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { addColor, getColor } = require("../Controllers/colorController");
+const { addColor, getColor } = require("../Controllers/ColorController");
 const router = express.Router();
 router.post("/color/add", addColor);
 router.get("/color/get", getColor);
